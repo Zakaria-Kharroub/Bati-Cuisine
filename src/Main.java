898b55ea2a83e128a5d0be51ed292aadc0502d96
zakaria <kharroubzakaria32@gmail.com>
2024-09-18 12:24:40 +0100

    connection avec la base de donnees

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import config.DbConnection;
 
 public class Main {
     public static void main(String[] args) {
+        DbConnection dbConnection = DbConnection.getInstance();
+        dbConnection.getConnection();
 
 
 
---
+++ src/config/DbConnection.java
@@ -0,0 +1,38 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+
+    private static DbConnection instance = null;
+    private Connection connection = null;
+
+    private DbConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BatiCuisine", "BatiCuisine", "");
+
+            if (connection != null) {
+                System.out.println("database connecter");
+            } else {
+                System.out.println("database pas connecter");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static DbConnection getInstance() {
+        if (instance == null) {
+            instance = new DbConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
+}
